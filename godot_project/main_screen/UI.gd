c91e458401569c3965dade98be057fda8057ad3c
Iria.Somobu <iillyyaa2033@mail.ru>
2021-02-08 01:21:52 +0300

    Working on main menu

--- godot_project/main_screen/UI.gd
+++ godot_project/main_screen/UI.gd
@@ -2,15 +2,45 @@ extends Control
 
 func core_data(data):
 	$AppLogo.text = data["logo"]["name"]
-	$MOTD.text = data["motd"]["name"]
-	$NewGame.text = data["ng"]["name"]
-	$Load.text = data["lg"]["name"]
-	$World.text = data["world"]["name"]
-	$Settings.text = data["sets"]["name"]
-	$Help.text = data["help"]["name"]
-	$Credits.text = data["titles"]["name"]
-	$Quit.text = data["exit"]["name"]
+	$Menu/MOTD.text = data["motd"]["name"]
+	$Menu/NewGame.text = data["ng"]["name"]
+	$Menu/Load.text = data["lg"]["name"]
+	$Menu/World.text = data["world"]["name"]
+	$Menu/Settings.text = data["sets"]["name"]
+	$Menu/Help.text = data["help"]["name"]
+	$Menu/Credits.text = data["titles"]["name"]
+	$Menu/Quit.text = data["exit"]["name"]
 	$Tips.bbcode_text = data["tips"]["tip"]
 
 func _ready():
-	$MOTD.grab_focus()
+	$Menu/MOTD.grab_focus()
+
+func hide_all_panels():
+	for n in $PanelContainer.get_children():
+		n.visible = false
+
+
+func switch_panel(name):
+	hide_all_panels();
+	
+	match name:
+		"motd":
+			show_panel_text("Motd here")
+		"newgame":
+			$PanelContainer/PanelNewgame.visible = true
+		"loadgame":
+			print("Loadgame")
+		"world":
+			print("World")
+		"settings":
+			print("settings")
+		"help":
+			print("help")
+		"credits":
+			show_panel_text("Credits here!")
+		"quit":
+			print("quit")
+
+func show_panel_text(data):
+	$PanelContainer/PanelText.bbcode_text = data;
+	$PanelContainer/PanelText.visible = true
