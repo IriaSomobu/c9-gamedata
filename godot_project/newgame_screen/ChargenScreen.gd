2f65bf3bfebb89bef803e7fd2793d6a16e5d8bf1
Iria.Somobu <iillyyaa2033@mail.ru>
2021-03-02 18:13:30 +0300

    Chargen: professions wip

--- godot_project/newgame_screen/ChargenScreen.gd
+++ godot_project/newgame_screen/ChargenScreen.gd
@@ -46,6 +46,7 @@ func char_data_ld(data):
 	for scenario in data["scenarios"]:
 		if scenario["is_default"]:
 			select_scenario(selected_scenario_idx)
+			set_default_prof_for_scenario(chargen_data["scenarios"][selected_scenario_idx]["id"])
 			break;
 		selected_scenario_idx += 1
 	
@@ -60,13 +61,21 @@ func select_scenario(idx):
 	selected_scenario_idx = idx;
 	var name_field = "name_m" if selected_gender_male else "name_f"
 	$StartProps/Scenario.text = chargen_data["scenarios"][idx][name_field];
-	set_default_prof_for_scenario(chargen_data["scenarios"][idx]["id"])
+
+
+func select_prof(id):
+	var prof = get_prof_by_id(id)
+	selected_profession_id = id
+	
+	var name_field = "name_m" if selected_gender_male else "name_f"
+	$StartProps/Profession.text = prof[name_field]
 
 
 func get_points_left():
 	var result = single_pool_limit + 0;
 	
 	result -= chargen_data["scenarios"][selected_scenario_idx]["cost"];
+	result -= get_prof_by_id(selected_profession_id)["cost"];
 	
 	return result;
 
@@ -94,12 +103,9 @@ func get_default_prof_for_scenario(scenatio_id):
 	return get_prof_by_id(prof_id)
 
 
-func set_default_prof_for_scenario(scenario_id):	
+func set_default_prof_for_scenario(scenario_id):
 	var prof = get_default_prof_for_scenario(scenario_id);
-	selected_profession_id = prof["id"]
-	
-	var name_field = "name_m" if selected_gender_male else "name_f"
-	$StartProps/Profession.text = prof[name_field]
+	select_prof(prof["id"])
 
 
 func update_points_label():
@@ -114,17 +120,27 @@ func _on_Sex_item_selected(id):
 	selected_gender_male = (id == 0);
 	rand_name();
 	select_scenario(selected_scenario_idx) # Reload scenario selection to update gender text
+	select_prof(selected_profession_id)
 
 
 func _on_Scenario_pressed():
 	$ScenarioPopup.show_panel(selected_scenario_idx);
 
+
 func _on_Profession_pressed():
-#	$ScenarioPopup.show_panel(selected_scenario_idx);
-	$ProfessionPopup.popup();
+	var profs = chargen_data["profs"];
+	var ids = chargen_data["scenarios"][selected_scenario_idx]["permitted_profs"];
+	$ProfessionPopup.show_panel(profs, ids, selected_profession_id);
+
 
 func _on_ScenarioPopup_scenario_selected(idx):
 	select_scenario(idx)
+	set_default_prof_for_scenario(chargen_data["scenarios"][idx]["id"])
+	update_points_label()
+
+
+func _on_ProfessionPopup_on_profession_selected(id):
+	select_prof(id)
 	update_points_label()
 
 
--- godot_project/newgame_screen/ProfessionPopup.gd
+++ godot_project/newgame_screen/ProfessionPopup.gd
@@ -4,14 +4,58 @@ signal on_profession_selected(id)
 
 onready var core = get_node("/root/Root").core;
 
+var loaded_sex_male = true
+var loaded_profs = []
 var selected_id = "";
 
 func _ready():
 	pass
 
+func show_panel(profs, permitted_ids, _selected_id):
+	
+	var selected_idx = 0
+	selected_id = _selected_id;
+	
+	loaded_profs = []
+	var idx = 0
+	for id in permitted_ids:
+		for prof in profs:
+			if prof["id"] == id:
+				loaded_profs.append(prof)
+		
+		if id == _selected_id:
+			selected_idx = idx
+		
+		idx += 1
+	
+	
+	var is_male = get_parent().selected_gender_male
+	loaded_sex_male = is_male
+	var name_field = "name_m" if is_male else "name_f"
+	
+	$Panel/Items.clear();
+	for prof in loaded_profs:
+		$Panel/Items.add_item(prof[name_field])
+	
+	$Panel/Items.select(selected_idx)
+	select(selected_idx)
+	popup();
+
+
+func select(idx):
+	var prof = loaded_profs[idx]
+	selected_id = prof["id"]
+	
+	var desc_field = "desc_m" if loaded_sex_male else "desc_f"
+	var descr = "[color=#5C5]" + prof[desc_field] + "[/color]"
+	var cost = core.tr("Cost:") + " [color=grey]" + str(prof["cost"]) + "[/color]"
+	
+	var text = descr + "\n\n" + cost
+	$Panel/InfoText.bbcode_text = text;
+
 
 func _on_Items_item_selected(index):
-	pass # Replace with function body.
+	select(index)
 
 
 func _on_Items_item_activated(index):
